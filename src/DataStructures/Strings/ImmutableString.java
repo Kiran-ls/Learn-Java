29a653c1bc2c08b77379dcc8bf44922544f14014
Kiran-ls <kiranls759@gmail.com>
2025-04-15 12:53:24 +0530

    Update

---
+++ src/DataStructures/Strings/ImmutableString.java
@@ -0,0 +1,15 @@
+package DataStructures.Strings;
+
+import java.util.Scanner;
+
+public class ImmutableString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        String s3 = s1.concat(s2); //or s1 + s2;
+        System.out.println("Original UerName: " + s1);
+        System.out.println("New UserName: " + s3);
+        sc.close();
+    }
+}
---
+++ src/DataStructures/Strings/StringPool.java
@@ -0,0 +1,17 @@
+package DataStructures.Strings;
+
+public class StringPool {
+    public static void main(String[] args) {
+        String s1 = "Mango";
+        String s2 = new String("Apple");
+
+        String s3 = "Mango";
+        String s4 = new String("Apple");
+
+        System.out.println(s1 == s3); //is 100 = 100?
+        System.out.println(s1.equals(s3)); //is mango = mango?
+
+        System.out.println(s2 == s4); //is 200 = 300?
+        System.out.println(s2.equals(s4)); //is apple = apple?
+    }
+}
