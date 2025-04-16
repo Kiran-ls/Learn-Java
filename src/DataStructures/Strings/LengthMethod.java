b0abb0bd177e35ddef9ad1489873dbcbbbdf2c37
Kiran-ls <kiranls759@gmail.com>
2025-04-16 23:31:56 +0530

    Update

---
+++ src/DataStructures/Strings/LengthMethod.java
@@ -0,0 +1,13 @@
+package DataStructures.Strings;
+
+import java.util.Scanner;
+
+public class LengthMethod {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = sc.nextLine();
+        System.out.println("Length of the string is " + str.length() + ".");
+        sc.close();
+    }
+}
