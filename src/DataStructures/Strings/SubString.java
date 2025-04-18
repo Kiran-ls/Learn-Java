2c3a49f7503888bb03f0724b8402988f8e73f8e2
Kiran-ls <kiranls759@gmail.com>
2025-04-18 23:14:29 +0530

    Added code

---
+++ src/DataStructures/Strings/SubString.java
@@ -0,0 +1,19 @@
+package DataStructures.Strings;
+
+import java.util.Scanner;
+
+public class SubString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a first string:");
+        String str1 = sc.nextLine();
+        System.out.println("enter a second string:");
+        String str2 = sc.nextLine();
+
+        if (str1.contains(str2)) {
+            System.out.println("The substring '" + str2 + "' present in the main string");
+        } else {
+            System.out.println("The substring '" + str2 + "' is not present in the main string");
+        }
+    }
+}
