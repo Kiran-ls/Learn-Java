34d0bc51f817b9fef732b732221bfbe356a97240
Kiran-ls <kiranls759@gmail.com>
2025-04-18 23:19:34 +0530

    Added code

---
+++ src/DataStructures/Strings/FindCharacter.java
@@ -0,0 +1,17 @@
+package DataStructures.Strings;
+
+import java.util.Scanner;
+
+public class FindCharacter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String str = sc.nextLine();
+        System.out.println("Enter a position:");
+        int position = sc.nextInt();
+
+        //find the character at specific position
+        System.out.println("Character at position " + position + ": " + str.charAt(position-1));
+        sc.close();
+    }
+}
